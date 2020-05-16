cb5db5baec8273dda5fcabfeb930781ade22faee
Sonny-2k <Sonny-2k@users.noreply.github.com>
2020-05-16 15:21:27 +0700

    First time using gitignore and sayHi function from dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,11 @@
-function Dog(){
+function Dog(name){
 	this.stomache=[];
+	this.name = name;
 }
 Dog.prototype.eat = function (cat) {
 	this.stomache.push(cat);
+}
+Dog.prototype.sayHi = function () {
+	console.log("Hello world , I'am dog and my name is "+ chalk.yellow(this.name));
 }
 module.exports = Dog;
--- mouse.js
+++ mouse.js
@@ -1,8 +1,4 @@
 function Mouse(kind){
 	this.kind=kind;
-	this.dead=false;
-}
-Mouse.prototype.die = function () {
-	this.dead=true;
 }
 module.exports=Mouse;
